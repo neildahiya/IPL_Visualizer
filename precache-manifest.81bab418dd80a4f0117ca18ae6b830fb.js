a5735964cc56cecfc9e44371e8ebd06107ebf2b3
Neil Dahiya <neildahiya1@gmail.com>
2020-04-28 03:32:22 +0530

    Updates

---
+++ precache-manifest.81bab418dd80a4f0117ca18ae6b830fb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374ee411bc4ba1767809",
+    "url": "/css/app.93091dbb.css"
+  },
+  {
+    "revision": "99eb6d1ace158db2831c",
+    "url": "/css/chunk-0a5a7a26.ee31dbbf.css"
+  },
+  {
+    "revision": "4e9c6e06fa60b92ffd08",
+    "url": "/css/chunk-ebd5d9ea.04ae002e.css"
+  },
+  {
+    "revision": "bd1d9c7cf015b3ad2fac",
+    "url": "/css/chunk-vendors.c0cf1705.css"
+  },
+  {
+    "revision": "476543cc12032252216dc7b8b538f16c",
+    "url": "/img/logo.476543cc.png"
+  },
+  {
+    "revision": "e40606755330da5c92aa85f738b8372f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "374ee411bc4ba1767809",
+    "url": "/js/app.7621d81e.js"
+  },
+  {
+    "revision": "99eb6d1ace158db2831c",
+    "url": "/js/chunk-0a5a7a26.61abe9ae.js"
+  },
+  {
+    "revision": "b9292b73aa3930bf24f9",
+    "url": "/js/chunk-2d0e6884.1c948641.js"
+  },
+  {
+    "revision": "99fcf0c2d671c62d9485",
+    "url": "/js/chunk-2d0e9b82.f7ad6ee2.js"
+  },
+  {
+    "revision": "4e9c6e06fa60b92ffd08",
+    "url": "/js/chunk-ebd5d9ea.4ddfc6b8.js"
+  },
+  {
+    "revision": "bd1d9c7cf015b3ad2fac",
+    "url": "/js/chunk-vendors.28fd9556.js"
+  },
+  {
+    "revision": "0a22863ded69792d560c8551386f9df7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
