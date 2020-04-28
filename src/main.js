2c3166d0327ddf89383c99bdf266d71518494eee
Neil Dahiya <neildahiya1@gmail.com>
2020-04-28 11:50:03 +0530

    router added

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import "bootstrap/dist/css/bootstrap.css";
 import BootstrapVue from "bootstrap-vue/dist/bootstrap-vue.esm";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import './registerServiceWorker'
+import router from './router'
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+  router,
+  render: (h) => h(App)
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <router-view />
+    <Dashboard />
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import Dashboard from "../components/Dashboard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Dashboard,
+  },
+};
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  /* background-color: cornflowerblue; */
 }
 </style>
